6e9d6adab3b30d99203d4e27877006cfef7f5ffc
Akkey01 <akshatmishrajiii@gmail.com>
2025-05-20 00:58:52 -0400

    changes to image

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,3 +1,4 @@
+// src/components/Summary/Summary.jsx
 import React from 'react';
 import styles from "./Summary.module.css";
 import { getImageURL } from '../../utils';
@@ -6,25 +7,37 @@ export default function Summary() {
   return (
     <section className={styles.container} id="summary">
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm <span className={styles.name}>Akshat Mishra</span></h1>
-        <h2 className={styles.headline}>Data Scientist | AI Product Developer | MS CE @NYU</h2>
-        {/* <p className={styles.headline-img} >
-          <p>
-              <img src="https://readme-typing-svg.herokuapp.com?color=2A2A2A&duration=3000&pause=500&size=18&center=false&vCenter=true&lines=Data+Scientist+;AI/ML+Product+Developer+;MS+CS+at+New+York+University+;"/>
-          </p>
-        </p> */}
+        <h1 className={styles.title}>
+          Hi, I'm <span className={styles.name}>Akshat Mishra</span>
+        </h1>
+        <h2 className={styles.headline}>
+          Data Scientist | AI Product Developer | MS CE @NYU
+        </h2>
         <p className={styles.summary}>
-          Bridging the gap between data and impactful solutions! 
+          Bridging the gap between data and impactful solutions!
         </p>
         <p className={styles.additional}>
           Actively pursuing Data Science/Software roles
         </p>
         <div className={styles.BtnsContainer}>
-          <a className={styles.Btns} href='/Akshat_Mishra_Resume_Data_Science.pdf' download>View My Resume</a>
-          {/* <a className={styles.icon} href='mailto:arsalan.anwar.7777@gmail.com'><img src={getImageURL("contact/emailIcon.png")} alt="Email" hidden/></a> */}
+          <a
+            className={styles.Btns}
+            href="/Akshat_Mishra_Resume_Data_Science.pdf"
+            download
+          >
+            View My Resume
+          </a>
         </div>
       </div>
-      <img className={styles.summaryImg} src={getImageURL("summary/Akshat_Mishra.jpg")} alt='Summary Image' />
+
+      <div className={styles.imageWrapper}>
+        <img
+          className={styles.summaryImg}
+          src={getImageURL("summary/Akshat_Mishra.jpg")}
+          alt="Akshat Mishra"
+        />
+      </div>
+
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
